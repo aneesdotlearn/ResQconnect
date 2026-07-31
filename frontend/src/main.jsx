ffc90f0a864b17933cb4aeeb4bdceb3028086590
aneesdotlearn <aneesdotlearn.com>
2026-07-31 17:16:00 +0530

    Implement authentication workflow

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
 import App from './App';
 import { store } from './store';
 import './index.css';
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster position="top-center" toastOptions={{ duration: 3500 }} />
     </Provider>
   </React.StrictMode>
 );
